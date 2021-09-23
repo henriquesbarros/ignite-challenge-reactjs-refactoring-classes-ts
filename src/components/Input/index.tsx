df0b9c69d6cadd2757507b1947062a918bb3f13e
Henrique Barros <henriquesbarroshs@gmail.com>
2021-09-22 21:45:53 -0300

    challenge finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,19 +3,26 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
+import { IconType } from 'react-icons/lib';
+
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: IconType
+}
+
+export default function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
-  const { fieldName, defaultValue, registerField } = useField(name);
+  const { fieldName, defaultValue, registerField } = useField(name || '');
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -49,5 +56,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
